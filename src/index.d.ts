bd3c1b22b9dc089967ffd245236d3ac5a1eb5468
Benjamin Eckel <bhelx@simst.im>
2023-12-19 14:05:08 -0600

    add prelude, fix problems

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,5 @@
 declare module 'main' {
   export function run(): I32;
+  export function compileTemplate(): I32;
+  export function render(): I32;
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,42 @@
 import { renderToString } from 'react-dom/server';
 import React from 'react'
 import * as reactTools from 'react-tools'
+import * as Prelude from './prelude'
+
+function getGlobals() {
+  const keys = Object
+    .getOwnPropertyNames(Prelude)
+    .filter(k => k !== "default")
+    .sort();
+  const refs = keys.map(k => Prelude[k])
+  return [keys, refs]
+}
+
+export function compileTemplate() {
+  const inputStr = Host.inputString()
+  const { name, code } = JSON.parse(inputStr)
+  Var.set(`template-${name}`, code)
+}
+
+export function render() {
+  const { name, props } = JSON.parse(Host.inputString())
+  const code = JSON.parse(Var.get(`template-${name}`))
+  let view = reactTools.transform(code, {}) + '; return App'
+  let [globalKeys, globalRefs] = getGlobals()
+  globalKeys.push('React')
+  globalRefs.push(React)
+  globalKeys.push(view)
+  view = (new Function(...globalKeys))(...globalRefs)
+  Host.outputString(renderToString(React.createElement(view, props, null)))
+}
 
 export function run() {
-  const inp = JSON.parse(Host.inputString())
-  let { view, props } = inp
-  view = reactTools.transform(view, {}) + '; return App'
-  view = (new Function('React', view))(React)
+  let { view, props } = JSON.parse(Host.inputString())
+  view = reactTools.transform(view, {}) + '; return App;'
+  let [globalKeys, globalRefs] = getGlobals()
+  globalKeys.push('React')
+  globalRefs.push(React)
+  globalKeys.push(view)
+  view = (new Function(...globalKeys))(...globalRefs)
   Host.outputString(renderToString(React.createElement(view, props, null)))
 }
